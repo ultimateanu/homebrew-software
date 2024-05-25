71298d8ac59e4b5fef73f0eee16ed267247d9302
Bandi, Anirudh <ultimateanu@gatech.edu>
2024-05-25 16:35:23 -0700

    update hashdir to v1.3.2

--- Formula/hashdir.rb
+++ Formula/hashdir.rb
@@ -1,8 +1,8 @@
 class Hashdir < Formula
   desc "Command-line utility to checksum directories and files"
   homepage "https://ultimateanu.github.io/hashdir/"
-  url "https://github.com/ultimateanu/hashdir/archive/refs/tags/v1.3.1.tar.gz"
-  sha256 "36fad59153a387ba0592ae0f62d1006b788a9f4155240400f1cf9be2c844790b"
+  url "https://github.com/ultimateanu/hashdir/archive/refs/tags/v1.3.2.tar.gz"
+  sha256 "b75a448cad1ff1fdd484b1de8ced1e26ff9eedf70cb24a64cce19db5978fe9b7"
   license "MIT"
 
   depends_on "dotnet"
